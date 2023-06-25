3e875bcdd1f5c0f7ba2ed45b45a5595e2baa10e6
Anas Najaa <spidernet12@gmail.com>
2023-06-25 20:46:34 +0300

    Added new problem 1168.py strings

---
+++ 1008.py
@@ -0,0 +1,20 @@
+# LEDS of numbers 0 1 2 3 4 5 6 7 8 9
+numbers = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
+
+rounds = int(input())
+counter = 1
+
+numbers_to_leds = [None] * rounds
+
+while counter <= rounds:
+    numbers_to_leds[counter-1] = input()
+    leds_nums = list(numbers_to_leds[counter-1])
+
+    total = 0
+
+    for num in leds_nums:
+        total += numbers[int(num)]
+
+    print("{number} leds".format(number=total))
+
+    counter = counter + 1
